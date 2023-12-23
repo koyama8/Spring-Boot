d73a3aa3c1d3852fec8d25f6f9002000317f0b2d
Matheus Koyama <koyamaCRS@gmail.com>
2023-12-23 20:01:40 -0300

    Inserindo a Listagem na API

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -2,6 +2,9 @@ package med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * MedicoRepository por estar herdando os do JPA, possui todos os metodo de salvar,incluir e mostrar
+ */
 public interface MedicoRepository extends JpaRepository<Medico,Long> {
 
 }
